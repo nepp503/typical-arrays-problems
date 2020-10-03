101fa00def142ebd502d21f709e4a518b03ea8a3
Vitali Siniak <43549823+nepp503@users.noreply.github.com>
2020-10-03 15:49:31 +0200

    feat: add min, max and avg functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length==0 ){
+    return 0;
+  }
+  let length = array.length;
+  let min = array[0];
+  for (let i = 1; i<length; i++){
+    if(array[i]<min){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length==0 ){
+    return 0;
+  }
+  let length = array.length;
+  let max = array[0];
+  for (let i = 1; i<length; i++){
+    if(array[i]>max){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length==0 ){
+    return 0;
+  }
+  let length = array.length;
+  let sum = 0;
+  for (let i = 0; i<length; i++){
+    sum += array[i];
+  }
+  return (sum/length);
 }
